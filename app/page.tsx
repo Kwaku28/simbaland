d45130d3f1195bd421dec07a14c973bb28057454
Kwaku28 <rekenburg777@gmail.com>
2025-04-17 22:52:50 +0000

    Add mobile navigation to view

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Approach from "@/components/Approach";
 import Ecosystem from "@/components/Ecosystem";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import MobileNavbar from "@/components/MobileNav";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
 
@@ -10,7 +11,8 @@ export default function Home() {
   return (
     <main className="relative flex justify-center items-center flex-col sm:px-10 px-5 overflow-clip">
       <div className="w-full">
-        <FloatingNav navItems={navItems} />
+        <FloatingNav navItems={navItems} className="hidden md:flex" />
+        <MobileNavbar />
         <Hero />
         <About />
         <Ecosystem />
