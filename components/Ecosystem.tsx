e12e7fbdf2abdb3822895cefd4741e9338917a22
Kwaku28 <rekenburg777@gmail.com>
2025-04-17 02:11:03 +0000

    Display the timeline feature

---
+++ components/Ecosystem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+import { ecoData } from "@/data/ecosystem";
+
+const Ecosystem = () => {
+  
+  return (
+    <div className="relative w-full overflow-clip" id="ecosystem">
+      <Timeline data={ecoData} />
+    </div>
+  );
+};
+
+export default Ecosystem;
