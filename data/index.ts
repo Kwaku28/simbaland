d4c3cd49acd673b4dbfe6d61001801df5305edde
Kwaku28 <rekenburg777@gmail.com>
2025-04-17 02:04:11 +0000

    Create index.ts

---
+++ data/index.ts
@@ -0,0 +1,12 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Ecosystem", link: "#ecosystem" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const flipWords = [
+  "Traders",
+  "Builders",
+  "Investors"
+]
+
