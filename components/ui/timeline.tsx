23b5dfb7cb7a36e5bf48c0cdae0795670b2391ea
Kwaku28 <rekenburg777@gmail.com>
2025-05-18 21:58:24 +0000

    Update ecosystem with logos

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -1,14 +1,11 @@
 "use client";
-import {
-  useScroll,
-  useTransform,
-  motion,
-} from "motion/react";
+import { useScroll, useTransform, motion } from "motion/react";
 import React, { useEffect, useRef, useState } from "react";
 
 interface TimelineEntry {
   title: string;
   content: React.ReactNode;
+  icon: string;
 }
 
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
@@ -41,8 +38,8 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           Our Ecosystem
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          Come on a journey with us as we build the future of Africa&apos;s crypto
-          landscape.
+          Come on a journey with us as we build the future of Africa&apos;s
+          crypto landscape.
         </p>
       </div>
 
@@ -53,8 +50,12 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+              <div className="h-16 absolute left-0 md:left-0 w-16 rounded-full bg-white dark:bg-black flex items-center justify-center">
+                <img
+                  src={item.icon}
+                  alt={item.title}
+                  className="size-16 object-contain rounded-full"
+                />
               </div>
               <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
                 {item.title}
