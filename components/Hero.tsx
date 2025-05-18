51d6dd8fd9089105e24e5a822d5e19e4c64e5f6e
Kwaku28 <rekenburg777@gmail.com>
2025-05-18 22:12:57 +0000

    Edit text sizes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,14 +18,14 @@ const Hero = () => {
       </div>
       <div className="pt-36 pb-20 lg:px-20">
         <div className="flex flex-col max-w-[89vw] md:max-w-2xl lg:max-w-[64vw] gap-4 absolute top-58 lg:top-38 z-10">
-          <h2 className="text-sm md:text-lg lg:text-2xl lg:mt-10 font-bold">
+          <h2 className="text-lg md:text-xl lg:text-2xl lg:mt-10 font-bold">
             Welcome to <span className="text-purple">Simbaland</span>
           </h2>
-          <h2 className="uppercase tracking-widest text-start text-[16px] md:text-[22px]/10 lg:text-4xl/10 max-w-90 lg:max-w-[48vw]">
+          <h2 className="uppercase tracking-widest text-start text-[22px]/10 lg:text-4xl/10 max-w-90 lg:max-w-[48vw]">
             Explore Web3 With Confidence.
           </h2>
 
-          <p className="text-sm md:text-lg lg:text-2xl lg:max-w-[64vw]">
+          <p className="text-lg lg:text-2xl lg:max-w-[64vw]">
             Simbaland is your gateway to simplified crypto trading, education,
             and curated opportunities. We bring together practical tools, expert
             insights, and a thriving community — all in one platform.
