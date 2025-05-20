73006f6721294cfee682f5d438f264838a457971
Kwaku28 <rekenburg777@gmail.com>
2025-05-20 23:28:48 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -18,3 +18,22 @@ export const words = [
     text: "Avenues",
   },
 ];
+
+export const contact = [
+    {
+      email: "samuel.manu.sm@gmail.com",
+      name: "Samuel Duodu Manu",
+      designation: "CEO",
+      telegram: "@SamSmrt",
+      teleLink: "https://t.me/SamSmrt",
+      src: "",
+    },
+    {
+      email: "Lovemensah51@gmail.com",
+      name: "Godlove Mensah",
+      designation: "CTO",
+      telegram: "@pipsalpha1",
+      teleLink: "https://t.me/pipsalpha1",
+      src: "",
+    },
+  ];
