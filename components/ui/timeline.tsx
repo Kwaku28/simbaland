8198ad6028e3792451828adac3455c5be37ff3e5
Kwaku28 <rekenburg777@gmail.com>
2025-05-20 23:24:44 +0000

    Edit text color

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -34,10 +34,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+        <h2 className="text-lg md:text-4xl mb-4 text-white max-w-4xl">
           Our Ecosystem
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+        <p className="text-neutral-300 text-sm md:text-base max-w-sm">
           Come on a journey with us as we build the future of Africa&apos;s
           crypto landscape.
         </p>
@@ -50,7 +50,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-16 absolute left-0 md:left-0 w-16 rounded-full bg-white dark:bg-black flex items-center justify-center">
+              <div className="h-16 absolute left-0 md:left-0 w-16 rounded-full bg-white flex items-center justify-center">
                 <img
                   src={item.icon}
                   alt={item.title}
