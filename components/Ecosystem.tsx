c17a0784e0470384e97e7d8e29edb8036ea916f5
Kwaku28 <rekenburg777@gmail.com>
2025-05-24 19:08:36 +0000

    Remove padding around container

--- components/Ecosystem.tsx
+++ components/Ecosystem.tsx
@@ -3,9 +3,8 @@ import { Timeline } from "@/components/ui/timeline";
 import { ecoData } from "@/data/ecosystem";
 
 const Ecosystem = () => {
-  
   return (
-    <div className="relative w-full px-5 md:px-10 overflow-clip" id="ecosystem">
+    <div className="relative w-full overflow-clip" id="ecosystem">
       <Timeline data={ecoData} />
     </div>
   );
