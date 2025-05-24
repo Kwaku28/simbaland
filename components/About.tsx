f323c075314005c9e6a61cd4d31fc465161df55f
Kwaku28 <rekenburg777@gmail.com>
2025-05-24 19:09:16 +0000

    Add background image and update text color

--- components/About.tsx
+++ components/About.tsx
@@ -4,15 +4,20 @@ import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 const About = () => {
   return (
     <div
-      className="flex flex-col max-w-[89vw] h-screen items-center justify-center gap-4 lg:pt-20 pb-20 lg:px-20 px-5"
+      className="flex flex-col max-w-full h-screen items-center justify-center gap-4 lg:pt-20 pb-20 lg:px-20 px-5"
       id="about"
+      style={{
+        backgroundImage: "url('/about-background.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
       <div className="flex items-center justify-center border-5 border-purple p-3 mb-5 lg:mb-20">
-        <h2 className="heading">About us</h2>
+        <h2 className="heading text-black">About us</h2>
       </div>
-      <div className="flex flex-col md:grid md:grid-cols-2 md:gap-8 items-center justify-center">
+      <div className="flex flex-col md:grid md:grid-cols-2 items-center justify-center">
         <TypewriterEffectSmooth words={words} />
-        <p className="text-sm md:text-lg lg:text-xl lg:max-w-[64vw] text-neutral-100">
+        <p className="text-sm md:text-lg lg:text-xl lg:max-w-[64vw] text-black">
           Simbaland is an integrated ecosystem designed to help individuals
           navigate the complex world of Web3. Our platform hosts a suite of
           services built to empower users with knowledge, strategy, and early
