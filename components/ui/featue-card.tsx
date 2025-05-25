73896a61d273a65cfe20121dc3a86b09eaf0149f
Kwaku28 <rekenburg777@gmail.com>
2025-05-25 22:27:15 +0000

    Update text size for larger screens

--- components/ui/featue-card.tsx
+++ components/ui/featue-card.tsx
@@ -63,11 +63,11 @@ const Feature = ({
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-100">
+        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-100 2xl:text-3xl">
           {title}
         </span>
       </div>
-      <p className="text-sm text-neutral-300 max-w-xs relative z-10 px-10">
+      <p className="text-sm text-neutral-300 max-w-full 2xl:text-2xl relative z-10 px-10">
         {description}
       </p>
     </div>
