b323acbdd5435743213dd0a61dae1393fa3595e1
Kwaku28 <rekenburg777@gmail.com>
2025-05-27 23:33:21 +0000

    Resize the simbaland text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,16 +24,20 @@ const Hero = () => {
 
       <div className="px-5 lg:px-20 mt-20 relative z-10">
         <div className="flex flex-col max-w-[89vw] md:max-w-3xl lg:max-w-[64vw] gap-4">
-          <div className="w-32 md:w-44 lg:w-52 lg:h-10 xl:w-60 xl:h-12 2xl:w-72 2xl:h-14 xl:mt-10">
-            <Image
-              src="/simbaland-text.png"
-              alt="Simbaland Text"
-              width={250}
-              height={50}
-              className="size-full"
-              priority
-            />
-          </div>
+          <h2 className="flex items-end w-full gap-1 lg:gap-2 text-sm md:text-xl lg:text-2xl 2xl:text-4xl mt-5 lg:mt-10 font-bold text-neutral-100">
+            Welcome to
+            <div className="w-28 h-5 mb-0.5 md:mb-0 2xl:-mb-0.5 md:w-44 lg:w-52 lg:h-10 xl:w-60 2xl:w-72 2xl:h-14 xl:mt-10">
+              <Image
+                src="/simbaland-text.png"
+                alt="Simbaland Text"
+                width={200}
+                height={50}
+                className="size-full"
+                priority
+              />
+            </div>
+          </h2>
+
           <h2
             className={`${oswald.className} text-golden-brown uppercase tracking-widest font-bold text-start text-[22px]/10 md:text-4xl/14 lg:text-6xl/16 max-w-92 md:max-w-full lg:max-w-[52vw] 2xl:text-8xl/26`}
           >
